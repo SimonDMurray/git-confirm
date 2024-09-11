beda92ba281089182db6b209fb504d9029b91fc4
Simon Murray <simondonaldmurray@gmail.com>
2024-09-11 16:33:17 +0100

    removing interactivity of hook

--- hook.sh
+++ hook.sh
@@ -1,49 +1,5 @@
 #!/usr/bin/env bash
 
-# If we have a STDIN, use it, otherwise get one
-if tty >/dev/null 2>&1; then
-    TTY=$(tty)
-else
-    TTY=/dev/tty
-fi
-
-IFS=$'\n'
-
-# http://djm.me/ask
-ask() {
-    while true; do
-
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -n "$1 [$prompt] "
-
-        # Read the answer
-        read REPLY < "$TTY"
-
-        # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
-
-        # Check if the reply is valid
-        case "$REPLY" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
-
-    done
-}
-
 check_file() {
     local file=$1
     local match_pattern=$2
@@ -61,26 +17,22 @@ check_file() {
             echo "$matched_line"
         done
 
-        if ask "Include this in your commit?"; then
-            echo 'Including'
-        else
-            echo "Not committing, because $file matches $match_pattern"
-            exit 1
-        fi
+        echo "Not committing, because $file matches $match_pattern"
+        exit 1
     fi
 }
 
 # Actual hook logic:
 
-MATCH=$(git config --get-all hooks.confirm.match)
-if [ -z "$MATCH" ]; then
+patterns_to_match=$(git config --get-all hooks.confirm.match)
+if [ -z "$patterns_to_match" ]; then
     echo "Git-Confirm: hooks.confirm.match not set, defaulting to 'TODO'"
     echo 'Add matches with `git config --add hooks.confirm.match "string-to-match"`'
-    MATCH='TODO'
+    patterns_to_match='TODO'
 fi
 
 for file in `git diff --cached -p --name-status | cut -c3-`; do
-    for match_pattern in $MATCH
+    for match_pattern in $patterns_to_match
     do
         check_file $file $match_pattern
     done
